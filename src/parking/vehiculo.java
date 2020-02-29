f75fed69809e372bbf7fb169dc4879321256f3cc
RAGO <ragogarcia1@gmail.com>
2020-02-29 16:44:14 -0500

    Iniciando proyecto

---
+++ src/parking/vehiculo.java
@@ -0,0 +1,32 @@
+package parking;
+
+public class vehiculo {
+	private String placa;
+	private String tipo;
+	private String modelo;
+	
+	public String getPlaca() {
+		return placa;
+	}
+	
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	
+	public String getTipo() {
+		return placa;
+	}
+	
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+}
